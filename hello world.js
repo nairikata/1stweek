470a74f679e87467b4f572712db20fba5622fb49
unknown <your.email@yourmailserver.com>
2022-01-18 23:15:44 +0330

    1st week's js exercise

---
+++ hello world.js
@@ -0,0 +1,52 @@
+console.log("سلام دنیا");
+console.log("hello world")
+
+
+'use strict';
+
+let numberX;
+
+console.log("X is a integer number and equal to ");
+console.log(numberX);
+
+numberX = 13;
+
+console.log("X is a integer number and equal to ");
+console.log(numberX);
+
+let mystring="Nairika Tahmoures Zadeh";
+console.log("this is my full name");
+console.log(mystring);
+
+mystring="Siavash";
+console.log("My brother name is");
+console.log(mystring);
+
+let z=7.25;
+console.log(z);
+
+let a=Math.round(z);
+console.log(a);
+
+let b
+
+if (z>a){
+b=z
+}
+else{
+    b=a
+}
+;
+console.log(b);
+
+let names=[];
+console.log("my family names");
+console.log(names);
+let animales=["dog", "cat", "fish"];
+console.log(animales[1]);
+animales.unshift("piglet");
+console.log(animales[1]);
+
+let mysentence="Programming is so interesting!";
+console.log(mysentence.length);
+
